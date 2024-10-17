3d42a1266cf4a652e127be197d320b4930fd6988
imf <imoyaf@gmail.com>
2024-10-17 22:41:28 +0200

    Add configuration annotation to application

--- virtualpet/src/main/java/imf/virtualpet/VirtualpetApplication.java
+++ virtualpet/src/main/java/imf/virtualpet/VirtualpetApplication.java
@@ -1,9 +1,11 @@
 package imf.virtualpet;
 
+import imf.virtualpet.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class VirtualpetApplication {
 
 	public static void main(String[] args) {
