79751a1d45abd63edae16f27476e4c8c295883c0
imf <imoyaf@gmail.com>
2024-10-17 14:41:54 +0200

    Add service methods

--- virtualpet/src/main/java/imf/virtualpet/service/UserService.java
+++ virtualpet/src/main/java/imf/virtualpet/service/UserService.java
@@ -1,4 +1,31 @@
 package imf.virtualpet.service;
 
+import imf.virtualpet.entity.User;
+import imf.virtualpet.repository.UserRepository;
+
+import lombok.Data;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import reactor.core.publisher.Mono;
+
+@Data
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Mono<User> registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public Mono<User> loginUser(String userName, String password) {
+        return userRepository.findByUserName(userName)
+                .filter(user -> passwordEncoder.matches(password, user.getPassword()));
+    }
 }
