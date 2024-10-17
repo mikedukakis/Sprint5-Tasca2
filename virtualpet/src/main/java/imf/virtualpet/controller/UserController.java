572ba0766e591d826f3fdc7e4262895490db2095
imf <imoyaf@gmail.com>
2024-10-17 22:42:46 +0200

    Add endpoint to UserController class

--- virtualpet/src/main/java/imf/virtualpet/controller/UserController.java
+++ virtualpet/src/main/java/imf/virtualpet/controller/UserController.java
@@ -1,4 +1,25 @@
 package imf.virtualpet.controller;
 
+import imf.virtualpet.entity.User;
+import imf.virtualpet.service.UserService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController (UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/registerUser")
+    public Mono<User> registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
 }
