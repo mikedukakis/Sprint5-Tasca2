344d27345a8582e129ce718b23c339318f7c4b25
imf <imoyaf@gmail.com>
2024-11-02 21:32:23 +0100

    Create pet service class

---
+++ src/main/java/imf/virtualpet/domain/virtualpet/service/VirtualPetService.java
@@ -0,0 +1,49 @@
+package imf.virtualpet.domain.virtualpet.service;
+
+import imf.virtualpet.domain.virtualpet.dto.VirtualPetCreationRequestDTO;
+import imf.virtualpet.domain.virtualpet.entity.VirtualPet;
+import imf.virtualpet.domain.virtualpet.repository.VirtualPetRepository;
+import imf.virtualpet.domain.virtualpet.util.VirtualPetMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class VirtualPetService {
+    private final VirtualPetRepository virtualPetRepository;
+
+    public Flux<VirtualPet> findPetsByUsername(String username) {
+        return virtualPetRepository.findByOwnerUsername(username);
+    }
+
+    public Mono<VirtualPet> findPetByNameAndUsername(String name, String username) {
+        return virtualPetRepository.findByNameAndOwnerUsername(name, username);
+    }
+
+    public Mono<VirtualPet> createPet(VirtualPetCreationRequestDTO virtualPetCreationRequestDTO, String ownerUsername) {
+        VirtualPet virtualPet = VirtualPetMapper.fromCreationDTOToEntity(virtualPetCreationRequestDTO);
+        virtualPet.setOwnerUsername(ownerUsername);
+        return virtualPetRepository.save(virtualPet);
+    }
+
+    public Mono<Void> deletePetIfOwner(String petId, String username) {
+        return virtualPetRepository.findById(petId)
+                .filter(virtualPet -> virtualPet.getOwnerUsername().equals(username))
+                .flatMap(virtualPetRepository::delete);
+    }
+
+    public Mono<VirtualPet> findPet(String name) {
+        return virtualPetRepository.findByName(name);
+    }
+
+    public Flux<VirtualPet> findAllPets() {
+        return virtualPetRepository.findAll();
+    }
+
+    public Mono<Void> deletePet(String petId) {
+        return virtualPetRepository.deleteById(petId);
+    }
+
+}
