e8926cd381350f1a4e4b66a64abc925d548529e5
imf <imoyaf@gmail.com>
2024-11-02 20:06:18 +0100

    Add security classes (JWT authentication filter, security config and logout service

---
+++ src/main/java/imf/virtualpet/security/configuration/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package imf.virtualpet.security.configuration;
+
+import imf.virtualpet.configuration.JwtService;
+import imf.virtualpet.token.repository.TokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter implements WebFilter {
+
+    private final JwtService jwtService;
+    private final ReactiveUserDetailsService userDetailsService;
+    private final TokenRepository tokenRepository;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+        if (path.contains("/virtualpet/auth")) {
+            return chain.filter(exchange); // Skip filtering for auth routes
+        }
+        String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return chain.filter(exchange);
+        }
+
+        String jwt = authHeader.substring(7);
+        String username = jwtService.extractUsername(jwt);
+
+        if (username == null) {
+            return chain.filter(exchange);
+        }
+
+        return userDetailsService.findByUsername(username)
+                .switchIfEmpty(Mono.error(new RuntimeException("User not found")))
+                .flatMap(userDetails -> tokenRepository.findByToken(jwt)
+                        .map(token -> !token.isExpired() && !token.isRevoked())
+                        .defaultIfEmpty(false)
+                        .flatMap(isTokenValid -> {
+                            if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
+                                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                                        userDetails,
+                                        null,
+                                        userDetails.getAuthorities()
+                                );
+                                return chain.filter(exchange)
+                                        .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authToken));
+                            }
+                            return chain.filter(exchange); // Continue without authentication
+                        })
+                );
+    }
+}
---
+++ src/main/java/imf/virtualpet/security/configuration/SecurityConfiguration.java
@@ -0,0 +1,90 @@
+package imf.virtualpet.security.configuration;
+
+
+import imf.virtualpet.domain.user.entity.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutHandler;
+import org.springframework.security.web.server.authentication.logout.SecurityContextServerLogoutHandler;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+import reactor.core.publisher.Mono;
+
+import static imf.virtualpet.domain.user.entity.Permission.*;
+import static org.springframework.http.HttpMethod.*;
+
+@Configuration
+@EnableWebFluxSecurity
+@RequiredArgsConstructor
+@EnableReactiveMethodSecurity
+public class SecurityConfiguration {
+
+    private static final String[] WHITE_LIST_URL = {
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html",
+            "/virtualpet/user/register",
+            "/virtualpet/user/login",
+            "/virtualpet/auth/**",
+    };
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final ReactiveAuthenticationManager authenticationManager;
+    private final ServerLogoutHandler logoutHandler = new SecurityContextServerLogoutHandler();
+
+    @Bean
+    public ReactiveAuthenticationManager reactiveAuthenticationManager(
+            ReactiveUserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        UserDetailsRepositoryReactiveAuthenticationManager manager =
+                new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
+        manager.setPasswordEncoder(passwordEncoder);
+        return manager;
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        AuthenticationWebFilter authenticationWebFilter = new AuthenticationWebFilter(authenticationManager);
+        authenticationWebFilter.setRequiresAuthenticationMatcher(ServerWebExchangeMatchers.pathMatchers("/api/v1/management/**"));
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges ->
+                        exchanges
+                                .pathMatchers(WHITE_LIST_URL).permitAll()
+                                .pathMatchers("/virtualpet/management/**").hasAuthority(Role.ROLE_ADMIN.name())
+                                .pathMatchers(GET, "/virtualpet/management/**").hasAuthority(ADMIN_READ.name())
+                                .pathMatchers(POST, "/virtualpet/management/**").hasAuthority(ADMIN_CREATE.name())
+                                .pathMatchers(PUT, "/virtualpet/management/**").hasAuthority(ADMIN_UPDATE.name())
+                                .pathMatchers(DELETE, "/virtualpet/management/**").hasAuthority(ADMIN_DELETE.name())
+                                .anyExchange().authenticated()
+                )
+                .authenticationManager(authenticationManager)
+                .addFilterAt(authenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .addFilterBefore(jwtAuthFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .logout(logout -> logout
+                        .logoutUrl("/virtualpet/auth/logout")
+                        .logoutHandler(logoutHandler)
+                        .logoutSuccessHandler((exchange, authentication) ->
+                                Mono.just("logout")
+                                        .doOnTerminate(ReactiveSecurityContextHolder::clearContext)
+                                        .then()
+                        )
+                )
+                .build();
+    }
+}
