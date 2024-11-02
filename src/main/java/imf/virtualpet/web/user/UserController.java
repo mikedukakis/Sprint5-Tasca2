13b3cf8ad175e4bd35e744291a01d33b4e458d30
imf <imoyaf@gmail.com>
2024-11-02 21:34:03 +0100

    Create controllers for user in general and Admin and Manager

---
+++ src/main/java/imf/virtualpet/web/user/UserController.java
@@ -0,0 +1,35 @@
+package imf.virtualpet.web.user;
+
+import imf.virtualpet.domain.user.service.UserService;
+import imf.virtualpet.domain.user.dto.ChangePasswordDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/virtualpet/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService service;
+
+    @PatchMapping
+    public ResponseEntity<?> changePassword(@RequestBody ChangePasswordDTO request, Principal connectedUser) {
+        service.changePassword(request, connectedUser);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/mypets")
+    public String myPetsPage() {
+        return "mypets";
+    }
+
+}
