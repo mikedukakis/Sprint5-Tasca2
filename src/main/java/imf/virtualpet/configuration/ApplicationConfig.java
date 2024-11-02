efbfd65a2f5ca69a45d5368307178e029f02adc3
imf <imoyaf@gmail.com>
2024-11-02 20:03:16 +0100

    Add configuration files for application, JWT service and Swagger. Add initial creation of collection and admin if not existing

---
+++ src/main/java/imf/virtualpet/configuration/ApplicationConfig.java
@@ -0,0 +1,39 @@
+package imf.virtualpet.configuration;
+
+import imf.virtualpet.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import reactor.core.publisher.Mono;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository repository;
+
+    @Bean
+    public ReactiveUserDetailsService userDetailsService() {
+        return username -> repository.findByUsername(username)
+                .switchIfEmpty(Mono.error(new UsernameNotFoundException("User not found")))
+                .cast(org.springframework.security.core.userdetails.UserDetails.class);
+    }
+
+    @Bean
+    public ReactiveAuthenticationManager authenticationManager(ReactiveUserDetailsService userDetailsService) {
+        UserDetailsRepositoryReactiveAuthenticationManager authManager = new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
+        authManager.setPasswordEncoder(passwordEncoder());  // Set password encoder for authentication
+        return authManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/imf/virtualpet/configuration/init/InitialDataLoader.java
@@ -0,0 +1,55 @@
+package imf.virtualpet.configuration.init;
+
+import imf.virtualpet.domain.user.entity.Role;
+import imf.virtualpet.domain.user.entity.User;
+import imf.virtualpet.domain.user.repository.UserRepository;
+import imf.virtualpet.security.auth.service.AuthenticationService;
+import imf.virtualpet.security.auth.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import reactor.core.publisher.Mono;
+
+@Configuration
+@RequiredArgsConstructor
+public class InitialDataLoader {
+
+    private final AuthenticationService authenticationService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            // Create "Admin" user if not exists
+            Mono<User> adminMono = userRepository.findByUsername("Admin")
+                    .switchIfEmpty(Mono.defer(() -> {
+                        var admin = RegisterRequest.builder()
+                                .username("Admin")
+                                .password(passwordEncoder.encode("password"))
+                                .role(Role.ROLE_ADMIN)
+                                .build();
+                        return authenticationService.register(admin)
+                                .doOnNext(response -> System.out.println("Admin token: " + response.getAccessToken()))
+                                .then(userRepository.findByUsername("Admin"));
+                    }));
+
+            // Create "Manager" user if not exists
+            Mono<User> managerMono = userRepository.findByUsername("Manager")
+                    .switchIfEmpty(Mono.defer(() -> {
+                        var manager = RegisterRequest.builder()
+                                .username("Manager")
+                                .password(passwordEncoder.encode("password"))
+                                .role(Role.ROLE_MANAGER)
+                                .build();
+                        return authenticationService.register(manager)
+                                .doOnNext(response -> System.out.println("Manager token: " + response.getAccessToken()))
+                                .then(userRepository.findByUsername("Manager"));
+                    }));
+
+            Mono.when(adminMono, managerMono).subscribe();
+        };
+    }
+}
