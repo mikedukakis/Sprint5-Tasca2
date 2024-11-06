738a5ad669d0b8db1413783426e3045f221a327d
imf <imoyaf@gmail.com>
2024-11-06 05:09:45 +0100

    Create global exception handler

--- src/main/java/imf/virtualpet/exception/GlobalExceptionHandler.java
+++ src/main/java/imf/virtualpet/exception/GlobalExceptionHandler.java
@@ -1,4 +1,24 @@
 package imf.virtualpet.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public Mono<Void> handleResponseStatusException(ResponseStatusException ex, ServerWebExchange exchange) {
+        exchange.getResponse().setStatusCode(ex.getStatusCode());
+        return exchange.getResponse().setComplete();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public Mono<Void> handleGenericException(Exception ex, ServerWebExchange exchange) {
+        exchange.getResponse().setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+        return exchange.getResponse().setComplete();
+    }
 }
