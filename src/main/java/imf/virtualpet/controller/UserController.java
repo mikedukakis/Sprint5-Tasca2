da9f885758796bb859cab77fb21595afd4254094
imf <imoyaf@gmail.com>
2024-11-07 03:50:06 +0100

    Add profile endpoint

--- src/main/java/imf/virtualpet/controller/UserController.java
+++ src/main/java/imf/virtualpet/controller/UserController.java
@@ -1,35 +1,38 @@
 package imf.virtualpet.controller;
 
+import imf.virtualpet.dto.UserProfileResponseDTO;
 import imf.virtualpet.service.UserService;
-import imf.virtualpet.dto.ChangePasswordDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
+import reactor.core.publisher.Mono;
 
 @Controller
 @RequestMapping("/virtualpet/user")
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService service;
-
-    @PatchMapping
-    public ResponseEntity<?> changePassword(@RequestBody ChangePasswordDTO request, Principal connectedUser) {
-        service.changePassword(request, connectedUser);
-        return ResponseEntity.ok().build();
-    }
+    private final UserService userService;
 
     @GetMapping("/login")
-    public String loginPage() {
-        return "login";
+    public Mono<String> loginPage() {
+        return Mono.just("login");
     }
 
     @GetMapping("/mypets")
-    public String myPetsPage() {
-        return "mypets";
+    public Mono<String> myPetsPage() {
+        return Mono.just("mypets");
+    }
+
+    @GetMapping("/profile")
+    public Mono<UserProfileResponseDTO> getUserProfile(@RequestParam String username) {
+        return userService.getUserByUsername(username)
+                .map(user -> {
+                    UserProfileResponseDTO response = new UserProfileResponseDTO();
+                    response.setUsername(user.getUsername());
+                    response.setRole(user.getRole());
+                    return response;
+                });
     }
 
 }
